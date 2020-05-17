78660af0d38ade2086525e19784a7d93eee9b89e
Shawnyboi <shawny@bu.edu>
2020-05-16 23:32:17 -0600

    spider AI

---
+++ PimpleGame/Assets/SpiderAI.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderAI : MonoBehaviour
+{
+    public SpiderMover mover;
+
+    public float idleTime = 2f;
+    public float walkTime = 2f;
+    enum spiderState
+    {
+        idle,
+        wandering,
+        chasing,
+        following
+    }
+
+    private spiderState currentState;
+    
+    private void Start()
+    {
+        currentState = spiderState.idle;
+        StartCoroutine(spiderStateMachine());
+    }
+
+    private IEnumerator handleIdleState()
+    {
+        float timePassed = 0f;
+        int rightOrLeftRoll = Random.Range(0, 2);
+        while (timePassed < idleTime)
+        {
+            timePassed += Time.deltaTime;
+            mover.turn(rightOrLeftRoll == 0, Time.deltaTime);
+
+            //determine next state
+            currentState = spiderState.wandering;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator handleWanderingState()
+    {
+        float timePassed = 0f;
+        while (timePassed < walkTime)
+        {
+            timePassed += Time.deltaTime;
+            mover.moveForward(Time.deltaTime);
+
+            //determine next state
+            currentState = spiderState.idle;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator handleChasingState()
+    {
+        yield return null;
+    }
+
+    private IEnumerator handleFollowingState()
+    {
+        yield return null;
+    }
+
+    private IEnumerator spiderStateMachine()
+    {
+        while (true)
+        {
+            switch (currentState)
+            {
+                case spiderState.idle:
+                    yield return StartCoroutine(handleIdleState());
+                    break;
+                case spiderState.wandering:
+                    yield return StartCoroutine(handleWanderingState());
+                    break;
+                case spiderState.chasing:
+                    yield return StartCoroutine(handleChasingState());
+                    break;
+                case spiderState.following:
+                    yield return StartCoroutine(handleFollowingState());
+                    break;
+                default:
+                    break;
+            }
+        }
+        yield return null;
+    }
+}
---
+++ PimpleGame/Assets/SpiderMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderMover : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float turnSpeed;
+    public Transform body;
+    public void moveForward(float timeDelta)
+    {
+        Vector3 newPos = this.transform.position + moveSpeed * Time.deltaTime * this.transform.forward;
+        body.position = newPos;
+    }
+
+    public void turn(bool turningRight, float timeDelta)
+    {
+        if (turningRight)
+        {
+            this.transform.Rotate(this.transform.up, turnSpeed * Time.deltaTime);
+        }
+        else
+        {
+            this.transform.Rotate(this.transform.up, -turnSpeed * Time.deltaTime);
+        }
+    }
+}
