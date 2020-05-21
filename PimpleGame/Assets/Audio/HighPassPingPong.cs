2a34c48a0afa6705f99d0d0509d1c905cbe6352e
bearwarp <hydr01987@yahoo.com>
2020-05-20 19:50:15 -0600

    audio testing
    audio in jozzScene

---
+++ PimpleGame/Assets/Audio/HighPassPingPong.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighPassPingPong : MonoBehaviour
+{
+    AudioHighPassFilter highPass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        highPass = GetComponent<AudioHighPassFilter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        highPass.cutoffFrequency = Mathf.PingPong(Time.time * 100, 600) + 200;
+    }
+}
